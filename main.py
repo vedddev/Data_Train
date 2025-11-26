2224f35d1e97de26c6ad93ac6b124c730f9c37ca
Vedant Shelake <vedantshelake28@gmail.com>
2025-11-26 20:48:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 path="F:\\Data sciance\\Project\\03_Gym\\RecGym.csv"
 df=pd.read_csv(path)
-
 print(df.isnull().sum())
-print(df.head(5))
+print(df.head(10))
 print(df)
+
