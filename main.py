67f8d92f3723b46d46bcfddb8dcb3a7639444e5e
Vedant Shelake <vedantshelake28@gmail.com>
2025-11-30 21:15:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import seaborn as sns 
 
 path="F:\\Data sciance\\Project\\03_Gym\\RecGym.csv"
 df=pd.read_csv(path)
 print(df.isnull().sum())
 print(df.head(10))
 print(df)
+
 
