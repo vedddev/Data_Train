c4beef9669c74696f45e103b29105b5d6af73503
Vedant Shelake <vedantshelake28@gmail.com>
2025-12-03 22:00:49 +0530

    Reformat main.py with consistent spacing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
+
 path="F:\\Data sciance\\Project\\03_Gym\\RecGym.csv"
 df=pd.read_csv(path)
 print(df.isnull().sum())
@@ -9,6 +10,7 @@ print(df.head(10))
 print(df)
 print(df.describe())
 print(df.column())
+
 
 
 
