eb942ee6ea8719197350d00e3c32d829b5898dd4
Vedant Shelake <vedantshelake28@gmail.com>
2025-12-05 09:42:06 +0530

    Remove extra blank lines in main.py

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
-
 path="F:\\Data sciance\\Project\\03_Gym\\RecGym.csv"
 df=pd.read_csv(path)
 print(df.isnull().sum())
 print(df.head(10))
-print(df)
 print(df.describe())
 print(df.column())
+
 
 
 
